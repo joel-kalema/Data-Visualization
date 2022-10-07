e5b3ac0413d99344007524eac26377927458ee37
andersonlebon <buyananderson@gmail.com>
2022-10-07 03:26:04 +0200

    Finishe the authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React, { useState, useEffect } from 'react';
 import Header from './componets/heder';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './componets/home';
 import Details from './componets/details';
 import './App.css';
@@ -9,10 +10,34 @@ import Details01 from './componets/details01';
 import Login from './componets/login';
 import SignUp from './componets/signUp';
 
+
+
+
 function App() {
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    token: '',
+
+  })
+   const navigate = useNavigate();
+  
+
+  useEffect(() => {
+    const token = localStorage.getItem('appToken')
+    if (token) {
+      navigate('/')
+      setState({token})
+    }
+    else {
+      navigate('/login')
+    }
+
+  }, [])
+
+  
   return (
     <div className="App">
-      <Router>
       <Header />
         <Routes>
           <Route path='/' element={ <Home />} />
@@ -23,7 +48,6 @@ function App() {
           <Route path='/login' element={ <Login /> } />
           <Route path='/signUp' element={ <SignUp /> } />
         </Routes>
-      </Router>
     </div>
   );
 }
--- src/componets/login.js
+++ src/componets/login.js
@@ -1,20 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import './login.css';
 
 const Login =() => {
+    const [state, setState] = useState({
+        email: '',
+        password: ''
+    })
+    const [error, setError] = useState('')
+    const navigate = useNavigate();
+
+    const handleSubmit = async(e) => {
+        setError('')
+        e.preventDefault();
+        const data = state
+
+        const url = 'http://localhost:3001/api/login'
+        const res = await axios({ method: 'post', url, data});
+        if (res.status === 200 && res.data.token !== undefined) {
+            localStorage.setItem('appToken', res.data.token)
+            navigate('/')
+            console.log(res.data.token);            
+        }
+        else {
+             setError(res.data)
+        }
+        
+    }
+    const handelChange = (e) => {
+        const stateMock = state;
+
+        const input = e.currentTarget.name
+        stateMock[input] = e.currentTarget.value
+        setState({...stateMock})
+    }
+    const {email, password} = state;
     return (
-        <div className="login">
+       <div className="login">
             <div>
-                <div>
+                 <div>
                     <h1>Connect your Industry</h1>
                 </div>
-                <form>
-                    <h2>Login</h2>
-                    <label for="username">Username:</label><br />
-                    <input type="text" id="username" name="username" /><br />
-                    <label for="pwd">Password:</label><br />
-                    <input type="password" id="pwd" name="pwd" /><br />
+                <div className='error'>
+                    {error}
+                </div>
+                <form  onSubmit={handleSubmit}>
+                    <label htmlFor="email">Email:</label><br />
+                    <input onChange={(e) => handelChange(e)} type="email" id="email" name="email" value={email} /><br />
+                    <label htmlFor="password">Password:</label><br />
+                    <input onChange={(e) => handelChange(e)} type="password" id="password" name="password" value={password}/><br />
                     <input type="submit" value="Submit" className='button'/>
                 </form>
                 <Link to='/signUp' >sign up</Link>
